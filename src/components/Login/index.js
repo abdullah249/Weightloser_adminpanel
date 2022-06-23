4a38cc6c4053e2f978ec7927920973e6eb197e31
Muhammad Abdullah <39337161+abdullah249@users.noreply.github.com>
2022-06-23 12:45:55 +0500

    3rd Update from Abdullah Side
    detail

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -39,7 +39,7 @@ const Login = () => {
       <Card>
         <Logo className={styles.logo} />
         <Typography className="mb-3" block variant="body_bold">
-          Login to your account check CI/CD 2nd update
+          Login to your account check CI/CD 3rd update
         </Typography>
         <Formik
           onSubmit={async (values) => {
