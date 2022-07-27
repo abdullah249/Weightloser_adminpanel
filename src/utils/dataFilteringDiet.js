e252ff2dd7a367d2001a615a0a6fc0451849bb82
Ameendarwan <ameen.darwan@agifly.be>
2022-07-28 04:32:57 +0500

    filtering based on phase

--- src/utils/dataFilteringDiet.js
+++ src/utils/dataFilteringDiet.js
@@ -4,46 +4,46 @@ const errorHandlingPhase = (el, i) => {
 };
 
 export const phaseBasedCalories1_3_4 = (data) => {
-  let result = data?.every((el, i) => {
-    return (Number(el.Calories) >= 1 && Number(el.Calories) <= 7) ||
-      (Number(el.Calories) >= 22 && Number(el.Calories) <= 42) ||
-      (Number(el.Day) >= 43 && Number(el.Calories) <= 70)
+  let result = data.every((el, i) => {
+    return (el.Day >= 1 && el.Day <= 7) ||
+      (el.Day >= 22 && el.Day <= 42) ||
+      (el.Day >= 43 && el.Day <= 70)
       ? el.MealType.toLowerCase() === "breakfast" &&
-        Number(el.Calories) >= 200 &&
-        Number(el.Calories) <= 400
+        el.Calories >= 200 &&
+        el.Calories <= 400
         ? true
         : el.MealType.toLowerCase() === "lunch" &&
-          Number(el.Calories) >= 400 &&
-          Number(el.Calories) <= 600
+          el.Calories >= 400 &&
+          el.Calories <= 600
         ? true
         : el.MealType.toLowerCase() === "snacks" &&
-          Number(el.Calories) >= 150 &&
-          Number(el.Calories) <= 300
+          el.Calories >= 150 &&
+          el.Calories <= 300
         ? true
         : el.MealType.toLowerCase() === "dinner" &&
-          Number(el.Calories) >= 400 &&
-          Number(el.Calories) <= 800
+          el.Calories >= 400 &&
+          el.Calories <= 800
         ? true
-        : console.log("YOYO1")
-      : Number(el.Calories) >= 8 && Number(el.Calories) <= 21
+        : errorHandlingPhase(el, i)
+      : el.Day >= 8 && el.Day <= 21
       ? el.MealType.toLowerCase() === "breakfast" &&
-        Number(el.Calories) >= 200 &&
-        Number(el.Calories) <= 400
+        el.Calories >= 200 &&
+        el.Calories <= 400
         ? true
         : el.MealType.toLowerCase() === "lunch" &&
-          Number(el.Calories) >= 400 &&
-          Number(el.Calories) <= 600
+          el.Calories >= 400 &&
+          el.Calories <= 600
         ? true
         : el.MealType.toLowerCase() === "snacks" &&
-          Number(el.Calories) >= 150 &&
-          Number(el.Calories) <= 300
+          el.Calories >= 150 &&
+          el.Calories <= 300
         ? true
         : el.MealType.toLowerCase() === "dinner" &&
-          //  Number(el.Carbs) === 0 &&
-          Number(el.Calories) >= 400 &&
-          Number(el.Calories) <= 800
+          el.Carbs === 0 &&
+          el.Calories >= 400 &&
+          el.Calories <= 800
         ? true
-        : console.log("YOYO2")
+        : errorHandlingPhase(el, i)
       : false;
   });
 
