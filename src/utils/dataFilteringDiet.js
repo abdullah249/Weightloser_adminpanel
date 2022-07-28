99115ce9a3a774806de01de2a7b2126738d8ceee
Ameendarwan <ameen.darwan@agifly.be>
2022-07-28 22:05:37 +0500

    Procedure and unique Name added

--- src/utils/dataFilteringDiet.js
+++ src/utils/dataFilteringDiet.js
@@ -1,13 +1,71 @@
-const errorHandlingPhase = (el, i) => {
-  console.log("Error in Calories", i, el.MealType, el.Calories, el.Day);
+const errorHandlingPhase = (el, i, arr) => {
+  console.log(
+    "Error in ",
+    "index =",
+    i,
+    ", Day =",
+    el.Day,
+    ", MealType =",
+    el.MealType,
+    ", Calories =",
+    el.Calories,
+    ", Procedure =",
+    el.Procedure,
+    ", arraylength =",
+    arr.length,
+    "Name =",
+    arr[i].Name,
+    arr[i + 1].Name,
+    arr[i].Name !== arr[i + 1].Name
+  );
   return false;
 };
 
+let sweetDishes = [
+  "Khoya",
+  "Sandesh",
+  "Boondi",
+  "Mishti Doi",
+  "Mohan Bhog",
+  "Lobongo",
+  "Nikruti",
+  "Roshogulla",
+  " Traditional Flan Custard",
+  "Tres Leches Pastel",
+  "Mexican Churros",
+  "Mexican Chocolate Sauce",
+  "Vietnamese Three Color Desserts",
+  "Che Ba Mau or Chè Ba Mau Chè Ba Màu",
+  "Che Bap or Chè Bắp – Sweet Corn Pudding",
+  "Che Troi Nuoc or Chè Trôi Nước",
+  "Che Chuoi or Chè Chuối",
+  " Vietnamese Sweet Corn Pudding",
+  "Vietnamese Sponge Cake",
+  "Vietnamese Fruit Cocktail",
+  "Tarta de Santiago",
+  "Tarta de Queso",
+  "Torrijas",
+  "Crema Catalana",
+  "Chocolate and Churros",
+  "Turrón",
+  "Mantecados",
+  "Polvorones",
+  "Flan",
+  "Baklava",
+  "Künefe",
+  "Lokum",
+  "Tavukgöğsü",
+  "Kadayif and Kunefe",
+  "Firinda Sutlac",
+  "Tavuk Gogsu",
+  "Asure",
+];
+
 export const phaseBasedCalories1_3_4 = (data) => {
-  let result = data.every((el, i) => {
-    return (el.Day >= 1 && el.Day <= 7) ||
-      (el.Day >= 22 && el.Day <= 42) ||
-      (el.Day >= 43 && el.Day <= 70)
+  let result = data.every((el, i, arr) => {
+    return (el.Day >= 1 && el.Day <= 7 && el.Procedure) ||
+      (el.Day >= 22 && el.Day <= 42 && el.Procedure) ||
+      (el.Day >= 43 && el.Day <= 70 && el.Procedure)
       ? el.MealType.toLowerCase() === "breakfast" &&
         el.Calories >= 200 &&
         el.Calories <= 400
@@ -24,8 +82,12 @@ export const phaseBasedCalories1_3_4 = (data) => {
           el.Calories >= 400 &&
           el.Calories <= 800
         ? true
-        : errorHandlingPhase(el, i)
-      : el.Day >= 8 && el.Day <= 21
+        : errorHandlingPhase(el, i, arr)
+      : el.Day >= 8 && el.Day <= 21 && el.Procedure && i < arr.length - 1
+      ? arr[i].Name !== arr[i + 1].Name
+        ? true
+        : errorHandlingPhase(el, i, arr)
+      : true
       ? el.MealType.toLowerCase() === "breakfast" &&
         el.Calories >= 200 &&
         el.Calories <= 400
@@ -43,17 +105,15 @@ export const phaseBasedCalories1_3_4 = (data) => {
           el.Calories >= 400 &&
           el.Calories <= 800
         ? true
-        : errorHandlingPhase(el, i)
-      : false;
+        : errorHandlingPhase(el, i, arr)
+      : errorHandlingPhase(el, i, arr);
   });
-
-  console.log("resultDoo", result);
+  console.log("Success", result);
+  return result;
 };
 
-export const phaseBasedCalories2 = (data) => {};
+export const checkSweetDishes = () => {};
 
 export const checkAllNutrition = () => {};
 
 export const checkAllProcedure = () => {};
-
-export const checkSweetDishes = () => {};
