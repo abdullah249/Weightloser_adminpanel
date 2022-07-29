84afc0820582a173c536fbccad6a44f34459e9ea
Ameendarwan <ameen.darwan@agifly.be>
2022-07-30 02:31:48 +0500

    sweet dishes

--- src/constants/sweetDishes.js
+++ src/constants/sweetDishes.js
@@ -1,27 +1,4 @@
-const errorHandlingPhase = (el, i, arr) => {
-  console.log(
-    "Error in ",
-    "index =",
-    i,
-    ", Day =",
-    el.Day,
-    ", MealType =",
-    el.MealType,
-    ", Calories =",
-    el.Calories,
-    ", Procedure =",
-    el.Procedure,
-    ", arraylength =",
-    arr.length,
-    "Name =",
-    arr[i].Name,
-    arr[i + 1].Name,
-    arr[i].Name !== arr[i + 1].Name
-  );
-  return false;
-};
-
-let sweetDishes = [
+export const sweetDishesList = [
   "Khoya",
   "Sandesh",
   "Boondi",
@@ -312,59 +289,3 @@ let sweetDishes = [
   "Chebakia",
   "Moroccan Sesame Cookies",
 ];
-
-export const phaseBasedCalories1_3_4 = (data) => {
-  let result = data.every((el, i, arr) => {
-    return (el.Day >= 1 && el.Day <= 7 && el.Procedure) ||
-      (el.Day >= 22 && el.Day <= 42 && el.Procedure) ||
-      (el.Day >= 43 && el.Day <= 70 && el.Procedure)
-      ? el.MealType.toLowerCase() === "breakfast" &&
-        el.Calories >= 200 &&
-        el.Calories <= 400
-        ? true
-        : el.MealType.toLowerCase() === "lunch" &&
-          el.Calories >= 400 &&
-          el.Calories <= 600
-        ? true
-        : el.MealType.toLowerCase() === "snacks" &&
-          el.Calories >= 150 &&
-          el.Calories <= 300
-        ? true
-        : el.MealType.toLowerCase() === "dinner" &&
-          el.Calories >= 400 &&
-          el.Calories <= 800
-        ? true
-        : errorHandlingPhase(el, i, arr)
-      : el.Day >= 8 &&
-        el.Day <= 21 &&
-        el.Procedure &&
-        arr[i].Name !== arr[i + 1].Name
-      ? el.MealType.toLowerCase() === "breakfast" &&
-        el.Calories >= 200 &&
-        el.Calories <= 400
-        ? true
-        : el.MealType.toLowerCase() === "lunch" &&
-          el.Calories >= 400 &&
-          el.Calories <= 600
-        ? true
-        : el.MealType.toLowerCase() === "snacks" &&
-          el.Calories >= 150 &&
-          el.Calories <= 300
-        ? true
-        : el.MealType.toLowerCase() === "dinner" &&
-          el.Carbs === 0 &&
-          el.Calories >= 400 &&
-          el.Calories <= 800
-        ? true
-        : errorHandlingPhase(el, i, arr)
-      : errorHandlingPhase(el, i, arr);
-  });
-  console.log("Success", result);
-  return result;
-};
-
-export const checkSweetDishes = () => {};
-
-export const checkAllNutrition = () => {};
-
-export const checkAllProcedure = () => {};
