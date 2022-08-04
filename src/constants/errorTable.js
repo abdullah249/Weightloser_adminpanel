60e591824994a473a989ca2a0cf6070d314910e2
MAmeenDarwan <ameen.darwan@wearenova.co.uk>
2022-08-04 05:20:59 +0500

    filtering

---
+++ src/constants/errorTable.js
@@ -0,0 +1,74 @@
+export const excelDietColumns = [
+  {
+    dataKey: "Day",
+    title: "Day",
+  },
+  {
+    dataKey: "DayName",
+    title: "DayName",
+  },
+  {
+    dataKey: "MealType",
+    title: "MealType",
+  },
+  {
+    dataKey: "Calories",
+    title: "Calories",
+  },
+  {
+    dataKey: "fat",
+    title: "Fat",
+  },
+  {
+    dataKey: "Protein",
+    title: "Protein",
+  },
+  {
+    dataKey: "Carbs",
+    title: "Total Carbs",
+  },
+  {
+    dataKey: "SR",
+    title: "Net Carbs",
+  },
+  {
+    dataKey: "SaturatedFat",
+    title: "SaturatedFat",
+  },
+  {
+    dataKey: "Category",
+    title: "Category",
+  },
+  {
+    dataKey: "Cuisine",
+    title: "Cuisine",
+  },
+  // {
+  //   title: "Joining Date",
+  //   render: (data) => {
+  //     return <span>{dayjs(data.JoiningDate).format("DD MMM, YYYY")}</span>;
+  //   },
+  // {
+  //   title: "Weight Lost",
+  //   render: (data) => <span>{data.StartWeight - data.CurrentWeight} kg</span>,
+  // },
+  // {
+  //   title: "Action",
+  //   class: "text-center",
+  //   render: (data) => {
+  //     return (
+  //       <div className={classNames(styles.actions)}>
+  //         <IconButton>
+  //           <Message />
+  //         </IconButton>
+  //         {/* <IconButton>
+  //           <PencilAlt />
+  //         </IconButton>
+  //         <IconButton>
+  //           <TrashAlt />
+  //         </IconButton> */}
+  //       </div>
+  //     );
+  //   },
+  // },
+];
